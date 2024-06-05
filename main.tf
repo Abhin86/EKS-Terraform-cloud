741ccc3517b705e78796c21e448d4aa1c8c73aec
Abhin <124777743+Abhin86@users.noreply.github.com>
2024-06-05 18:17:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ module "eks" {
       capacity_type  = "SPOT"
     }
   }
-  cluster_endpoint_public_access = false
+  cluster_endpoint_public_access = true
 
   # Cluster access entry
   # To add the current caller identity as an administrator
