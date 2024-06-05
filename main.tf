d82058fefb8c1b37e4562800629a4ddfa0c8db49
Abhin <124777743+Abhin86@users.noreply.github.com>
2024-06-05 10:30:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,12 +19,6 @@ variable "AWS_ACCESS_KEY" {
   sensitive   = true
 }
 
-# Define the AWS provider
-provider "aws" {
-  region = "us-east-1"
-  # access_key = var.AWS_ACCESS_KEY
-  # secret_key = var.AWS_SECRET_KEY
-}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
