e01a8fb7f9e57c7b63259b00b0c98f8e0758cfbb
root <root@Urolime>
2024-06-05 10:26:16 +0530

    first1

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "The AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_ACCESS_KEY" {
+  description = "The AWS Access key"
+  type        = string
+  sensitive   = true
+}
+
+# Define the AWS provider
+provider "aws" {
+  region = "us-east-1"
+  # access_key = var.AWS_ACCESS_KEY
+  # secret_key = var.AWS_SECRET_KEY
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
